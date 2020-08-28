f0f130ec1a3767eb0264cd4c385f678051cb289b
Samira <54632872+S-samira2020@users.noreply.github.com>
2020-08-29 01:15:25 +0600

    Add files via upload

---
+++ program8.py
@@ -0,0 +1,10 @@
+magicians = ['alice', 'david', 'carolina']
+for name in magicians:
+    print(name)
+print()
+names = ['samira', 'sakiba', 'sagor babu']
+for magician in names:
+    #print(name)
+    print(magician.title() + ' it was a great trick')
+    print("I can't wait to see your trick, " + magician.title() + "\n" )
+    print('Thnak you everyone. That was a great magic show!')
